ffe5184f39ea67f0977e72adec3fd07bbaff2e8d
Richard Lucas <richard@grupovoz.com.br>
2021-07-08 19:13:24 -0300

    comentarios e uso de arrays para inputs

--- main.py
+++ main.py
@@ -10,32 +10,33 @@ if __name__ == "__main__":
     qualidade = ctrl.Antecedent(universe, 'qualidade')
     servico = ctrl.Antecedent(universe, 'servico')
     print('Definindo o consequente...')
-    gorjeta = ctrl.Consequent(np.arange(0, 21, 1), 'gorjeta')
+    gorjeta = ctrl.Consequent(np.arange(0, 21, 1), 'gorjeta')    
 
-    print('Definindo as variáveis linguísticas...')
+    print('Definindo as funcoes de pertinencia...')
     qualidade.automf(number=3, names=['ruim', 'boa', 'saborosa'])
-    servico.automf(number=3, names=['ruim', 'aceitável', 'ótimo'])
-
-    print('Definindo as funções de pertinência...')
+    servico.automf(number=3, names=['ruim', 'aceitavel', 'otimo'])
     gorjeta['baixa'] = fuzz.sigmf(gorjeta.universe, 5, -1)
     gorjeta['media'] = fuzz.gaussmf(gorjeta.universe, 10, 3)
     gorjeta['alta'] = fuzz.pimf(gorjeta.universe, 10, 20, 25, 50)    
-
+    
+    # qualidade.view()
+    # servico.view()
     # gorjeta.view()
     # input('Enter')
 
     print('Definindo as regras...')
     regra1 = ctrl.Rule(qualidade['ruim'] | servico['ruim'], gorjeta['baixa'])
-    regra2 = ctrl.Rule(servico['aceitável'], gorjeta['media'])
-    regra3 = ctrl.Rule(qualidade['saborosa'] | servico['ótimo'], gorjeta['alta'])
+    regra2 = ctrl.Rule(servico['aceitavel'], gorjeta['media'])
+    regra3 = ctrl.Rule(qualidade['saborosa'] | servico['otimo'], gorjeta['alta'])    
 
     sistema_controle = ctrl.ControlSystem([regra1, regra2, regra3])
     sistema = ctrl.ControlSystemSimulation(sistema_controle)
 
-    sistema.input['qualidade'] = 9
-    sistema.input['servico'] = 8
+    # Inputs aceita um array numpy ou um valor inteiro
+    sistema.input['qualidade'] = np.array([3, 5, 10, 3])
+    sistema.input['servico'] = np.array([4, 5, 2, 2])
     sistema.compute()
 
     print(sistema.output['gorjeta'])
     gorjeta.view(sim=sistema)
-    input('Enter')
+    input('Pressione qualquer tecla...')
